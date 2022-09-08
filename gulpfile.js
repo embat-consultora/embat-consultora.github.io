91e1d84f17b089bcb997811830e87da4ee7f4c4d
Anto Pisciolari <antopiscio@Antos-MacBook-Pro.local>
2022-09-08 16:55:53 -0300

    adding blog

--- gulpfile.js
+++ gulpfile.js
@@ -38,11 +38,7 @@ gulp.task("html:build", function () {
       })
     )
     .pipe(
-      comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
+      comments(`antopiscio@gmail.com
     `)
     )
     .pipe(gulp.dest(path.build.dir))
